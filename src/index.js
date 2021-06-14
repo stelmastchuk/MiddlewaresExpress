5375c0b3a05bf6d5417274594d613686fbc35b4c
stelmastchuk <vitorstelmastchuk@gmail.com>
2021-06-14 11:53:34 -0300

    desafio completo

--- src/index.js
+++ src/index.js
@@ -2,27 +2,122 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
+const { default: validator } = require('validator');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
 const users = [];
 
+
+
+
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+
+    return response.status(404).json({error: "Mensage error"});
+  }
+
+  request.user = user;
+
+  return next();
+
+  
+
+  
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const{user} = request;
+  
+  if(user.todos.length < 10 && user.pro == false || user.pro == true){
+
+    return next();
+
+  }
+  
+  if(user.todos.length >= 10 && user.pro == false ){
+
+    return response.status(403).json({error: "Mensage error"});
+
+  }
+
+  
+
+
+
+ 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+     
+    const {username} = request.headers;
+    const {id} = request.params;
+
+    const user = users.find((user) => user.username === username);
+
+    if(!user){
+
+      return response.status(404).json({error: "Mensage error"});
+    } 
+
+    if ( validator.isUUID(id) == false ){
+
+
+      return response.status(400).json({error: "Mensage error"});
+
+    }
+
+   const todo = user.todos.find((todo) => todo.id == id);
+
+   if(!todo){
+
+   return response.status(404).json({error: "Mensage error"});
+
+
+   }
+
+
+   request.user = user;
+
+   request.todo = todo;
+
+   next();
+
+
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+
+  const {id} = request.params;
+
+
+      const user = users.find((user) => user.id === id);
+
+       if(!user){
+
+        return response.status(404).json({error: "Mensage error"});
+      } 
+
+
+      request.user = user;
+
+      next();
+
+     
+
+
+  
 }
 
 app.post('/users', (request, response) => {
